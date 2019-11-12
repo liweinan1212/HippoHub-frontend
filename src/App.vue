19597595a919637df5467c696a5c472161f7b13d
liweinan <304331029@qq.com>
2019-11-12 16:46:26 +0800

    一些更改

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button>按钮</el-button>
   </div>
 </template>
 
---
+++ src/components/RandomNumberGenerator.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>随机数字生成器</h1>
+        
+    </div>
+</template>
